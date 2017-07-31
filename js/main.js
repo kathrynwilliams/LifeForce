5c4e41d3c937fc3f489ad6a6cbce25a4fff32c97
kathrynwilliams <kathrynwilliams109@gmail.com>
2017-07-31 18:00:41 -0400

    Cleaning JS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
 
-  /* $(function () {
-    $('.hidden').css('display', 'none');
-  });
-  */
-
-  //waypoints
-
-
   //Scrollify
   $(function() {
     $.scrollify({
@@ -36,7 +28,7 @@ $(document).ready(function(){
       },
   		after:function(index,sections) {
         if((index+1) == sections.length) {
-          $(".arrow-down").hide();
+          $(".arrow-down").hide(); //makes arrow disappear on last section
         }
       }
     });
@@ -45,26 +37,6 @@ $(document).ready(function(){
     });
   });
 
-
-
-  //Read More clicks
-
-  /*$('.packages-button').click( function(){
-    $('.packages-section').hide();
-    $('.learn-more').show();
-    $('.package1').show();
-    $('.package2').fadeIn(1000);
-    $('.package3').fadeIn(2000);
-     $('.see-less-container').fadeIn(3000);
-  }); */
-
-  //See Less clicks
-
-  /* $('.see-less-button').click( function(){
-    $('.learn-more').hide();
-    $('.packages-section').show();
-  }); */
-
   //flickity carousel
 
   $('.carousel').flickity({
@@ -72,18 +44,5 @@ $(document).ready(function(){
     contain: true,
     cellSelector: '.gallery-image'
   });
-
-  /* $('.facilities-button').click( function(){
-    $('.photo-gallery-container').show (function(){
-      $('.facilities-button').fadeOut();
-    });
-  });
-
-  $('.x-button2').click( function(){
-    $('.photo-gallery-container').hide(function(){
-      $('.photo-gallery-container').fadeOut();
-      $('.facilities-button').show();
-    });
-  }); */
 
 });
